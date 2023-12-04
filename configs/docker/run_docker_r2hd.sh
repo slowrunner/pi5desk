abb012d00ab4f0d77e3f3a333e7df3bb41215611
slowrunner <slowrunner@users.noreply.github.com>
2023-12-04 11:35:06 -0500

    add user pi to docker ros

--- configs/docker/run_docker_r2hd.sh
+++ configs/docker/run_docker_r2hd.sh
@@ -2,4 +2,4 @@
 
 cd ~/pi5desk/ros2ws
 # --rm  remove containter after finished
-docker run -it --net=host  -v ~/pi5desk/ros2ws:/ros2ws --rm r2hd
+docker run -it --net=host  -v ~/pi5desk/ros2ws:/ros2ws -w /ros2ws --rm r2hd
--- configs/docker/run_r2hdp.sh
+++ configs/docker/run_r2hdp.sh
@@ -2,4 +2,4 @@
 
 cd ~/pi5desk/ros2ws
 # --rm    remove container after running
-docker run -it --net=host  -v ~/pi5desk/ros2ws:/ros2ws --rm r2hdp
+docker run -it --net=host  -v ~/pi5desk/ros2ws:/ros2ws -w /ros2ws --rm r2hdp
