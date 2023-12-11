a9fcff0da1d06d262561e23fdb10aa8de9194193
slowrunner <slowrunner@users.noreply.github.com>
2023-12-11 12:11:32 -0500

    voices over 50MB limit - removed

---
+++ systests/piper-tts/get_arctic_voice.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://huggingface.co/rhasspy/piper-voices/blob/v1.0.0/en/en_US/arctic/medium/en_US-arctic-medium.onnx
+wget https://huggingface.co/rhasspy/piper-voices/blob/v1.0.0/en/en_US/arctic/medium/en_US-arctic-medium.onnx.json
+
+
