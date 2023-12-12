a525491ec71fcaef31ac35fa3b9b491f4873bc0f
slowrunner <slowrunner@users.noreply.github.com>
2023-12-11 19:16:01 -0500

    no rate increase

--- systests/piper-tts/get_arctic_voice.sh
+++ systests/piper-tts/get_arctic_voice.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-wget https://huggingface.co/rhasspy/piper-voices/blob/v1.0.0/en/en_US/arctic/medium/en_US-arctic-medium.onnx
-wget https://huggingface.co/rhasspy/piper-voices/blob/v1.0.0/en/en_US/arctic/medium/en_US-arctic-medium.onnx.json
-
+# This will download the arctic voice
+echo 'Hello.  This is arctic medium voice' | piper  --model en_US-arctic-medium     --output_file /dev/null
 
--- systests/piper-tts/piper.sh
+++ systests/piper-tts/piper.sh
@@ -2,5 +2,5 @@
 
 
 pushd /home/pi/pi5desk/systests/piper-tts
-echo $1 | piper   --model en_US-arctic-medium.onnx    --output_raw | aplay -r 33075 -f S16_LE -t raw - 
+echo $1 | piper   --model en_US-arctic-medium.onnx    --output_raw | aplay -r 22050 -f S16_LE -t raw - 
 popd
