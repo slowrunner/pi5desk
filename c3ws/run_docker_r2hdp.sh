aae47d2115a78378549aa1e23c9ab90c29fd2a96
slowrunner <slowrunner@users.noreply.github.com>
2023-12-22 19:49:26 -0500

    run in priviliged mode for joystick device access

--- c3ws/run_docker_r2hdp.sh
+++ c3ws/run_docker_r2hdp.sh
@@ -4,5 +4,6 @@ cd ~/pi5desk/c3ws
 # --rm removes container after exit
 # --w working dir to start in
 echo -e "\n*** STARTING ROS 2 HUMBLE PLUS IN DOCKER ***"
-docker run -it --net=host  -v /home/pi:/home/pi -w /home/pi/pi5desk/c3ws --rm r2hdp
+docker run -it --net=host  -v /home/pi:/home/pi -v /dev/input:/dev/input \
+ -v /dev/bus/usb:/dev/bus/usb  -w /home/pi/pi5desk/c3ws --privileged --rm r2hdp
 echo -e "\n*** EXITED DOCKER ***"
